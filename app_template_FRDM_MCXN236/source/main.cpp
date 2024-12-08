f59740aa9977c5b9a0912c01e48573942495148a
Tedd OKANO <okano.akifumi@gmail.com>
2024-12-08 12:39:48 +0900

    N236 application template restored

---
+++ app_template_FRDM_MCXN236/source/main.cpp
@@ -0,0 +1,34 @@
+/** test code for r01lib
+ *
+ *  @author Tedd OKANO
+ *
+ *  Released under the MIT license License
+ */
+
+#include	"r01lib.h"
+
+DigitalOut	r( RED,   1 );
+DigitalOut	g( GREEN, 1 );
+DigitalOut	b( BLUE,  1 );
+
+int main( void )
+{
+	printf( "Hello, world!\r\n" );
+
+	while ( true )
+	{
+		printf( "*\r\n" );
+		r	= 0;
+		g	= 1;
+		b	= 1;
+		wait( 0.5 );
+		r	= 1;
+		g	= 0;
+		b	= 1;
+		wait( 0.5 );
+		r	= 1;
+		g	= 1;
+		b	= 0;
+		wait( 0.5 );
+	}
+}
