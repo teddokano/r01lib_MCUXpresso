b971c6f4932520c7e7ef263d6c16492ede844d5b
Tedd OKANO <okano.akifumi@gmail.com>
2024-12-11 03:36:21 +0900

    hello world sample got printf test for float

--- z_r01lib_demo_Hello_world_FRDM_MCXN236/source/main.cpp
+++ z_r01lib_demo_Hello_world_FRDM_MCXN236/source/main.cpp
@@ -6,10 +6,13 @@
  */
 
 #include	"r01lib.h"
+#include	<math.h>
 
 int main( void )
 {
-	printf( "Hello, world! (in C style)\r\n" );
+	printf( "Hello, world!\r\n" );
+	printf( "test for float print: %f\r\n", M_PI );
+	printf( "test for utf-8 print: %s\r\n", "℃" );
 
 	return 0;
 }
